69073a30dac1b00f30136988897b24593e86fef2
Your <you@example.com>
2023-03-13 09:06:07 +0600

    News Done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Home = () => {
+
+    const AllNews = useLoaderData();
+
     return (
         <div>
-            <h4>This is home</h4>
+            <h4>This is home {AllNews.length}</h4>
+            {
+                AllNews.map(news => <NewsSummaryCard
+                key={news._id}
+                news={news}
+                >
+                </NewsSummaryCard>)
+            }
         </div>
     );
 };
